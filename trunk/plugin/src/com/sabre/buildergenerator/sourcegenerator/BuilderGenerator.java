07780ac9ade973bc12cedc4ef681738a50747fd5
pwyrzycki <pwyrzycki@058d35f2-1437-11df-94bc-6d6fff283d55>
2010-02-21 20:41:48 +0000

    corrected plural to single algorithm
    git-svn-id: http://fluent-builders-generator-eclipse-plugin.googlecode.com/svn@67 058d35f2-1437-11df-94bc-6d6fff283d55

--- trunk/plugin/src/com/sabre/buildergenerator/sourcegenerator/BuilderGenerator.java
+++ trunk/plugin/src/com/sabre/buildergenerator/sourcegenerator/BuilderGenerator.java
@@ -287,8 +287,6 @@ public class BuilderGenerator {
             elementName = name.substring(0, name.length() - 3) + "f";
         } else if (name.endsWith("ees")) {
             elementName = name.substring(0, name.length() - 1);
-        } else if (name.endsWith("es")) {
-            elementName = name.substring(0, name.length() - 2);
         } else if (name.endsWith("s")) {
             elementName = name.substring(0, name.length() - 1);
         } else {
